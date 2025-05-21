e9293cc82461b2ee98525898b73ba6d11c488a2f
AlphaIsYour <alphrenoorz@gmail.com>
2025-05-21 11:07:10 +0700

    test

--- src/view/index.java
+++ src/view/index.java
@@ -33,7 +33,6 @@ public class index extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 0, 0));
         jLabel1.setText("Hello ini Renooo");
-        jLabel1.setText("Hello ini Rayyan");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
