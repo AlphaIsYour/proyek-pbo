6e3895e1b3f6eef6b721f2f13a627c1d9c58c7dd
aqilown <aqillaumarayusuf@gmail.com>
2025-06-06 13:36:46 +0700

    final

--- src/model/Database.java
+++ src/model/Database.java
@@ -3,7 +3,7 @@ package model;
 import java.sql.*;
 
 public class Database {
-    private static final String DB_URL = "jdbc:mysql://localhost:3307/cookingpapa?characterEncoding=UTF-8";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/cookingpapa?characterEncoding=UTF-8";
     private static final String USER = "root";
     private static final String PASS = "";
 
